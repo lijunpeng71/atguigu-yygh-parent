9ca5f1011bddc72349ab3e3d6cad5b674d4f1811
lijunpeng02 <lijunpeng02@meituan.com>
2022-09-09 15:51:32 +0800

    init

---
+++ atguigu-yygh-modules/atguigu-yygh-service-user/src/main/java/com/atguigu/study/yygh/UserApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.study.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lijunpeng02
+ * @date 2022年09月09日 15:10
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
