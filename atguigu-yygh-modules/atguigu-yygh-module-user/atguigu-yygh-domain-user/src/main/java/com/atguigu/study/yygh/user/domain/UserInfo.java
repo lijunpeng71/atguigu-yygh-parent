b64be880e6b741267e228dcc42884e1b13d72917
lijunpeng02 <lijunpeng02@meituan.com>
2022-09-09 17:49:01 +0800

    init

---
+++ atguigu-yygh-modules/atguigu-yygh-module-user/atguigu-yygh-domain-user/src/main/java/com/atguigu/study/yygh/user/domain/UserInfo.java
@@ -0,0 +1,15 @@
+package com.atguigu.study.yygh.user.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author lijunpeng02
+ * @date 2022年09月09日 17:31
+ */
+@Data
+@TableName("user_info")
+public class UserInfo implements Serializable {
+}
--- atguigu-yygh-modules/atguigu-yygh-module-user/atguigu-yygh-service-user/src/main/java/com/atguigu/study/yygh/user/controller/UserController.java
+++ atguigu-yygh-modules/atguigu-yygh-module-user/atguigu-yygh-service-user/src/main/java/com/atguigu/study/yygh/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.atguigu.study.yygh.user.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/admin/user")
 public class UserController {
+
+    //用户列表（条件查询带分页）
+    @GetMapping("{page}/{limit}")
+    public Result list(@PathVariable Long page,
+                       @PathVariable Long limit,
+                       UserInfoQueryVo userInfoQueryVo) {
+        Page<UserInfo> pageParam = new Page<>(page,limit);
+        IPage<UserInfo> pageModel =
+                userInfoService.selectPage(pageParam,userInfoQueryVo);
+        return Result.ok(pageModel);
+    }
 }
